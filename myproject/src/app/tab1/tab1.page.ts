d17bee2ec0de90c62d0cb89c5579342d892ef1b3
Dima Nesterenko <nesterenkoodmytro@gmail.com>
2024-02-16 00:32:10 +0200

    add edits

--- myproject/src/app/tab1/tab1.page.ts
+++ myproject/src/app/tab1/tab1.page.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput,IonItem,IonItemGroup, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent,IonicModule,IonInput,IonItem, IonButton, IonItemGroup],
 })
 export class Tab1Page {
   constructor() {}
+  a:number = 0;
+  b:number = 0;
+  c:number = 0;
+  d:number = 0;
+  ras(a:any, b:any, c:any){
+    try {
+      this.a=parseFloat(a);
+      this.b=parseFloat(b);
+      this.c=parseFloat(c);
+      this.d=0;
+      if ((isNaN(this.a)==true) || (isNaN(this.b)==true) || (isNaN(this.c)==true) )
+      {
+        throw new Error("Параметри - не числа!!!");
+      }
+      if (this.a % 2 === 0 && this.b % 2 === 0 && this.c % 2 === 0) {
+        this.d = this.a * this.b * this.c;
+    } else {
+        let sum = this.a + this.b + this.c;
+        this.d = sum * sum;
+    }
+    }
+     catch (error) {
+      console.log(error);
+    }
+  }
+
 }
--- myproject/src/app/tab2/tab2.page.ts
+++ myproject/src/app/tab2/tab2.page.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput,IonItem, IonButton } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import {IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent,IonicModule,IonInput,IonItem, IonButton],
 })
 export class Tab2Page {
 
